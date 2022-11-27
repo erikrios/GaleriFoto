77314be13374efa96f76dba5ee93fb9a4cc24840
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-11-27 11:56:28 +0700

    Implements gorilla mux router

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	log.Println("Server started on port 8181...")
+	if err := ServeAPI(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func ServeAPI() error {
+	handler := &eventServiceHandler{}
+
+	r := mux.NewRouter()
+	eventsRouter := r.PathPrefix("/events").Subrouter()
+
+	eventsRouter.Methods("GET").Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
+
+	eventsRouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
+
+	eventsRouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
+
+	return http.ListenAndServe(":8181", r)
+}
+
+type eventServiceHandler struct{}
+
+func (e *eventServiceHandler) findEventHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (e *eventServiceHandler) allEventHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (e *eventServiceHandler) newEventHandler(w http.ResponseWriter, r *http.Request) {
+
+}
